06bb5b0a7910738975225b34e1a31404e7f5e6a1
lingfliu <lingf.liu@gmail.com>
2015-01-22 23:05:39 +0800

    removed src/ at root directory

--- src/string_ex.h
+++
@@ -1,9 +0,0 @@
-#ifndef _STRING_EX_
-#define _STRING_EX_
-
-#include <stdlib.h>
-#include <string.h>
-char* strtrim(char* str);
-char* strsub(char* str, char* substr, int start, int stop);
-int stridx(char* str, int idx_start, char parser);
-#endif
