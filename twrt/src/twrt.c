6bf7df10b55ee7f218f088fb7fa1b732f64a4cee
lingfliu <lingf.liu@gmail.com>
2015-02-07 19:26:27 +0800

    add synchronization and req message handling functions

--- twrt/src/twrt.c
+++ twrt/src/twrt.c
@@ -1,44 +1,60 @@
 #include "twrt.h"
 
 int main(int argn, char* argv[]){
-    //1. reset the config
-    //2. check the network
-    //3. retrieve authorization
-    //4. 
-    //5. retrieve stamps
-    //6. check the znet and retrieve the node status
-    //7. synchronization with the web
-    //8. start threads
+    //1. get config
+    /////////////////////////////////////
     get_config(&cfg);
     serial_config(cfg->serial_name, cfg->serial_type, cfg->serial_baudrate, &serial);
-    inet_client_config(cfg->server_ip, cfg->server_port, cfg->, &inet_client);
-
-    //initialize buffers
-    buffer_serial = create_buffer_byte_ring(BUFF_RING_LEN);
-    buffer_inet_client = create_buffer_byte_ring(BUFF_RING_LEN);
+    inet_client_config(cfg->server_ip, cfg->server_port, cfg->server_proc, &inet_client);
     
-    //initialize queue
-    msg = malloc(sizeof(struct_message));
-    msg->data = malloc(sizeof(char));
-    message_queue_init(msg_queue);
-    msg_stamp = 0; 
+    //2. initialize buffers, paras, and sys
+    /////////////////////////////////////
+    buffer_byte_ring_create(buff_serial, BUFF_RING_LEN);
+    buffer_byte_ring_create(buff_client, BUFF_RING_LEN);
+    memset(read_serial, 0, sizeof(read_serial));
+    memset(read_client, 0, sizeof(read_client));
+    message_queue_init(msg_q_rx);
+    message_queue_init(msg_q_tx);
+    msg_stamp = 0;
+    //time_net_hb = time();
+    //time_sys_reset = time();
 
+    serial_open(&serial);
+    while(inet_client_connect(&client)!= 0)
+	sleep(1); //sleep 1 second and try again
 
+    //3. retrieve authorization
+    /////////////////////////////////////
+    if(sys_get_auth(sys, client.fd)!=0){
+	perror("Device not authorized, please check your liscense file\n");
+	return -1;
+    }
+    //5. retrieve stamps from the web
+    /////////////////////////////////////
+    sys_get_stamp(sys, client.fd);
+
+    //6. initialize synchronization to the server and the local znet 
+    /////////////////////////////////////
+    sys_sync_init(sys, serial.fd, client.fd);
+
+    //7. start threads
+    
     pthread_mutex_init(&mut_serial);
-    pthread_mutex_init(&mut_inet_client);
-    pthread_mutex_init(&mut_msg_handler);
     pthread_cond_init(&cond_serial);
-    pthread_cond_init(&cond_inet_client);
-    //pthread_cond_init(&cond_msg_handler);
+    pthread_mutex_init(&mut_client);
+    pthread_cond_init(&cond_client);
+    pthread_mutex_init(&mut_msg_rx);
+    pthread_cond_init(&cond_msg_rx);
+    pthread_mutex_init(&mut_msg_tx);
+    pthread_cond_init(&cond_msg_tx);
 
     pthread_create(thrd_serial_rx, run_serial_rx, NULL);
-    pthread_create(thrd_inet_client_rx, run_inet_client, NULL);
-    pthread_create(thrd_translate_serial, run_serial, NULL);
-    pthread_create(thrd_translate_serial, run_translate_serial, NULL);
-    pthread_create(thrd_translate_inet_client, run_translate_inet_client, NULL);
-    pthread_create(thrd_msg_handler, run_msg_handler, NULL);
-    
-    pthread_create(thrd_sys, run_sys, NULL);
+    pthread_create(thrd_client_rx, run_client, NULL);
+    pthread_create(thrd_trans_serial, run_trans_serial, NULL);
+    pthread_create(thrd_trans_client, run_trans_client, NULL);
+    pthread_create(thrd_sys_msg_rx, run_sys_msg_rx, NULL);
+    pthread_create(thrd_sys_msg_tx, run_sys_msg_tx, NULL);
+    pthread_create(thrd_sys_ptask, run_sys_ptask, NULL);
 
     for(;;);
 }
@@ -64,71 +80,76 @@ void *run_serial_rx(){
     }
 }
 
-void *run_inet_client_rx(){
+void *run_client_rx(){
     int len;
     while(1){
 	usleep(1000);
-	pthread_mutex_lock(&mut_inet_client);
-	len = read(int_client->fd, read_inet_client, INET_BUFF_LEN);//non-blocking reading, return immediately
+	pthread_mutex_lock(&mut_client);
+	len = read(client->fd, read_client, INET_BUFF_LEN);//non-blocking reading, return immediately
 	if(len>0){
-	    put_buffer_byte_ring(buffer_inet_client, read_inet_client, len);
-	    pthread_cond_signal(&cond_inet_client);
+	    put_buffer_byte_ring(buff_client, read_client, len);
+	    pthread_cond_signal(&cond_client);
 	}else if(errno == EAGAIN | errno == EINTR ){
 	    //continue
 	}else if(len == 0){//if disconnected, reconnect
-	    while(inet_client_connect(inet_client)<0)
+	    while(inet_client_connect(client)<0)
 		sleep(1);// wait 1 s and try connect again
 	}else{
-	    while(inet_client_connect(inet_client)<0)
+	    while(inet_client_connect(client)<0)
 		sleep(1);// wait 1 s and try connect again
 	}
-	pthread_mutex_unlock(&mut_inet_client);
+	pthread_mutex_unlock(&mut_client);
     }
 }
 
-void *run_translate_serial(){
+void *run_trans_serial(){
+    struct_message *msg;
+    msg = message_create_empty();
     while(1){
 	usleep(1000);
 	pthread_mutex_lock(&mut_serial);
 	pthread_cond_wait(&cond_serial, &mut_serial);
 	//translate bytes into message
-	while(bytes2msg(buff_serial, msg_serial)>0){
+	while(bytes2msg(buff_serial, msg)>0){
 	    usleep(1);
-	    pthread_mutex_lock(&mut_msg_handler);
-	    message_queue_put(&msg_queue, msg_serial);
-	    pthread_mutex_unlock(&mut_msg_handler);
+	    pthread_mutex_lock(&mut_msg_rx);
+	    message_queue_put(msg_q_rx, msg);
+	    message_del(msg);
+	    pthread_mutex_unlock(&mut_msg_rx);
 	}
 	pthread_mutex_unlock(&mut_serial);
     }
 }
 
-void *run_translate_inet_client(){
+void *run_trans_client(){
+    struct_message *msg;
     while(1){
 	usleep(1000);
-	pthread_mutex_lock(&mut_inet_client);
-	pthread_cond_wait(&cond_inet_client, &mut_inet_client);
+	pthread_mutex_lock(&mut_client);
+	pthread_cond_wait(&cond_client, &mut_client);
 	//translate bytes into message
-	while(bytes2msg(buff_inet_client, msg_inet_client)>0){
+	while(bytes2msg(buff_client, msg)>0){
 	    usleep(1);
-	    pthread_mutex_lock(&mut_msg_handler);
-	    message_queue_put(&msg_queue, msg_inet_client);
-	    pthread_mutex_unlock(&mut_msg_handler);
+	    pthread_mutex_lock(&mut_msg_rx);
+	    message_queue_put(msg_q_rx, msg);
+	    message_flush(msg);
+	    pthread_mutex_unlock(&mut_msg_rx);
 	}
-	pthread_mutex_unlock(&mut_inet_client);
+	pthread_mutex_unlock(&mut_client);
     }
 }
 
-void *run_msg_handler(){
+void *run_sys_msg_rx(){
     struct_message *msg;
     message_flush(msg);
     while(1){
 	usleep(1000);
-	pthread_mutex_lock(&mut_msg_handler);
-	if(message_queue_isempty(msg_queue))
+	pthread_mutex_lock(&mut_msg_rx);
+	if(message_queue_isempty(msg_q_rx))
 		continue;
 	else
-	    message_queue_get(msg_queue, msg);
-	pthread_mutex_unlock(&mut_msg_handler);//unlock the msg_queue first
+	    message_queue_get_head(msg_q_rx, msg);
+	pthread_mutex_unlock(&mut_msg_rx);//unlock msg_q_rx first
 	if(msg != NULL){
 	    handle_msg(msg);
 	    message_flush(msg);
@@ -136,13 +157,83 @@ void *run_msg_handler(){
     }
 }
 
+void *run_sys_msg_tx(){
+    struct_message *msg;
+    struct_message_queue msg_q_item;
+    while(1){
+	usleep(1000);
+	pthread_mutex_lock(&mut_msg_tx);
+	if(message_queue_isempty(msg_q_tx))
+	    continue;
+	else{
+	    msg_q_item = message_queue_head(msg_q_tx);
+	    while(message_is_req(msg_q_item->msg))//if is req msg
+	    if(mesg_q_item->msg->timer == NULL){//unsend req
+		msg_q_item->msg->timer = time();
+		break;//when finish sending the first unsend msg, break
+	    }else{
+		msg_q_item = msg_q_item->next;
+		//if send before
+		//go to the next message
+	    }
+
+	    //skipped all send req msg and locate the first unsend msg
+	    //identify the write direction
+	    switch(message_direction(msg_q_item->msg)){
+		case MSG_DIR_SERIAL:
+		    pthread_create(thrd_serial_tx, run_serial_tx, msg);
+		    pthread_join(thrd_serial_tx);
+		    break;
+		case MSG_DIR_INET_CLIENT:
+		    pthread_create(thrd_client_tx, run_serial_tx, msg);
+		    pthread_join(thrd_client_tx);
+		    break;
+		default://if unknown, flush the message from the queue
+		    if(msg_q_item == msg_q_tx)
+			message_queue_flush(msg_q_tx);
+		    else
+			message_queue_flush(msg_q_item);
+		    break;
+	    }
+	}
+	pthread_mutex_unlock(&mut_msg_tx);
+    }
+}
+
 void* run_sys_ptask(){
+    time_t timer;
+    time_t timer_net_hb = time();
+    time_t timer_sys_reset = time();
+
+    struct_message_queue* msg_q_item;
     struct_message* msg;
+
     while(1){	
 	//periodic tasks
-	sleep(3);
-	//send the hb to the server
+	msleep(100);
+	timer = time();
 
+	//msg_q_tx clean
+	pthread_mutex_lock(&mut_msg_tx);
+	while(msg_q_tx->next!=msg_q_tx){
+	    message_queue_read(msg_q_tx, msg);
+	    if(difftime(timer, timer_net_hb)>TIME_INTERVAL_REQ && message_type(msg)==MSG_TYPE_REQ){
+		//get the req message 
+		on_req_failed(msg);
+		//remove the req message from the queue
+		message_queue_flush(msg_q_tx, 1);
+	    }
+	}
+	pthread_mutex_unlock(&mut_msg_tx);
+
+	//heart beat packet
+	if(difftime(timer, timer_net_hb)>TIME_INTERVAL_HB){
+	    message_create(msg, MSG_NET_HB);
+	    pthread_mutex_lock(&mut_msg_tx);
+	    message_queue_put(msg_q_tx, msg);//send the hb to the server
+	    timer_net_hb = time();//refresh the timer
+	    pthread_mutex_unlock(&mut_msg_tx);
+	}
 	   
     }
 }
@@ -186,7 +277,7 @@ int handle_msg(struct_message *msg){
 	    pthread_mutex_unlock(&mut_msg_handler);
 	    free(bytes);
 	    break;
-	case DATA_TYPE_ACK:
+	case DATA_TYPE_NET_HB:
 	    result = 0;
 	    //do nothing for the ack
 	    break;
--- twrt/src/twrt.h
+++ twrt/src/twrt.h
@@ -36,45 +36,53 @@ static pthread_t thrd_client_tx;
 static pthread_t thrd_trans_serial;
 static pthread_t thrd_trans_client;
 
-static pthread_t thrd_sys_msg_handler;
+static pthread_t thrd_sys_msg_rx; //thread for reading incoming messages
+static pthread_t thrd_sys_msg_tx; //thread for sending scheduled messages
 
 static pthread_t thrd_sys_ptask; //thread for performing periodic sys task
 
 //mut and cond
-static pthread_mutex_t mut_serial;
+static pthread_mutex_t mut_serial; // lock for buffer_serial
 static pthread_cond_t cond_serial;
-static pthread_mutex_t mut_client;
+static pthread_mutex_t mut_client; // lock for buffer_client
 static pthread_cond_t cond_client;
-static pthread_mutex_t mut_msg_handler;
-static pthread_cond_t cond_msg_handler;
+static pthread_mutex_t mut_msg_rx; //lock for msg_q_rx
+static pthread_cond_t cond_msg_rx;
+static pthread_mutex_t mut_msg_tx; //lock for msg_q_tx
+static pthread_cond_t cond_msg_tx;
 
 //runnables
 void *run_serial_rx();
-void *run_inet_client_rx();
-void *run_translate_serial();
-void *run_translate_inet_client();
-void *run_sys_msg_handler();
+void *run_client_rx();
+void *run_trans_serial();
+void *run_trans_client();
+void *run_sys_msg_rx();
+void *run_sys_msg_tx();
 void *run_sys_ptask();
 
 //buffers
 buffer_byte_ring *buff_serial;
 buffer_byte_ring *buff_client;
 char read_serial[BUFF_RW_LEN];
-char write_serial[BUFF_RW_LEN];
+//char write_serial[BUFF_RW_LEN];
 char read_client[BUFF_RW_LEN];
-char write_client[BUFF_RW_LEN];
+//char write_client[BUFF_RW_LEN];
 
 //message queue
 static struct_message *msg_serial;
 static struct_message *msg_client;
-static struct_message_queue* msg_queue;
-static struct_message_queue* msg_queue_req;
-static long msg_stamp;
+static struct_message_queue* msg_q_rx;
+static struct_message_queue* msg_q_tx;
+static long msg_tx_stamp;
 
 //functions
 int handle_msg(struct_message *msg);
+void on_req_failed(struct_message *msg);
 
 //timer for the sys_ptask
-static time_t time_net_hb;
-static time_t time_sys_reset;
+static time_t timer_net_hb;
+static time_t timer_sys_reset;
+
+#define TIME_INTERVAL_HB 1;
+#define TIME_INTERVAL_REQ 1;
 #endif
