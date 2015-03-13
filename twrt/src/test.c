e943a0be46e36c50b54c449d04faedef82eee5ac
lingfliu <lingf.liu@gmail.com>
2015-03-13 17:30:08 +0800

    regular op for test

--- twrt/src/test.c
+++ twrt/src/test.c
@@ -4,13 +4,13 @@
 #include <stdlib.h>
 #include <string.h>
 #include "buffer_ex.h"
-
+//
 void main(){
     //buffer_ring_byte test
     
     buffer_ring_byte *buff;
-    buff = buffer_ring_byte_create(buff, 16);
-    char a[9] = "123456789";
+    buff = buffer_ring_byte_create(buff, 6);
+    char a[16] = "123456789abcdefg";
     char *b;
     char *c;
     //b = realloc(b, 7);
@@ -18,13 +18,13 @@ void main(){
     printf("Read=%s\n",b);
     int m;
     for(m = 0; m < 16; m++){
-	buffer_ring_byte_put(buff,a,9);
-	b = buffer_ring_byte_get(buff,b,3);
-	c = buffer_ring_byte_read(buff,b,9);
+	buffer_ring_byte_put(buff,&a[m],1);
+	b = buffer_ring_byte_get(buff,b,1);
+	c = buffer_ring_byte_read(buff,b,1);
 	printf("Len=%d\t",buffer_ring_byte_len(buff));
-	printf("Buff=%s\t",buff->data);
+	printf("Buff=%s\n",buff->data);
 	printf("Get=%s\t",b);
-	printf("Read=%s\n",c);
+	//printf("Read=%s\n",c);
     }
 
     //test the size of time_t
