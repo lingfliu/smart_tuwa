d30fda2b102856f0df9ef8ed6872e2bf95f4b2d1
Lingfliu <lingfliu@gmail.com>
2015-09-04 13:50:20 +0800

    added operation functions for server socket

--- twrt/src/simple_inet.c
+++ twrt/src/simple_inet.c
@@ -6,18 +6,16 @@ int inet_client_config(char* ip, int port, int proc_type, inet* client){
     switch(client->proc){
 	case INET_PROC_TCP:
 	    if((client->fd = socket(AF_INET, SOCK_STREAM, 0)) < 0){
-		perror("create socket failed");
-		return -1;
+			return -1;
 	    }
 	    break;
 	case INET_PROC_UDP:
 	    if((client->fd = socket(AF_INET, SOCK_DGRAM, 0)) < 0){
-		perror("create socket failed");
-		return -1;
+			return -1;
 	    }
 	    break;
+		//if unknown proc, return error
 	default:
-	    perror("Unknown proctocol");
 	    return -1;
     }
 
@@ -28,9 +26,10 @@ int inet_client_config(char* ip, int port, int proc_type, inet* client){
     client->ip_addr.sin_family = AF_INET;
     client->ip_addr.sin_port = htons(port);
     if(inet_pton(AF_INET, ip, (void*)&client->ip_addr.sin_addr)<= 0){
-	perror("ip error\n");
-	return -1;
+		return -1;
     }
+
+
     return 0;
 }
 
@@ -39,27 +38,99 @@ int inet_client_connect(inet *client){
 }
 
 int inet_client_close(inet* client){
-	int retval;
-	retval = close(client->fd);
-	//reset the socket fd
-	switch(client->proc){
-		case INET_PROC_TCP:
-			if((client->fd = socket(AF_INET, SOCK_STREAM, 0)) < 0){
-				perror("create socket failed\n");
-				return -1;
-			}
-			break;
-		case INET_PROC_UDP:
-			if((client->fd = socket(AF_INET, SOCK_DGRAM, 0)) < 0){
-				perror("create socket failed\n");
-				return -1;
-			}
-			break;
-		default:
-			perror("Unknown proctocol\n");
-			return -1;
+	if (close(client->fd) < 0 ){
+		return -1;
 	}
-	int flags = fcntl(client->fd, F_GETFL, 0);
-	fcntl(client->fd, F_SETFL, flags | O_NONBLOCK);//set the socket as non-block 
+	else {
+		//reset the socket fd
+		switch(client->proc){
+			case INET_PROC_TCP:
+				if((client->fd = socket(AF_INET, SOCK_STREAM, 0)) < 0){
+					return -1;
+				}
+				break;
+			case INET_PROC_UDP:
+				if((client->fd = socket(AF_INET, SOCK_DGRAM, 0)) < 0){
+					return -1;
+				}
+				break;
+			default:
+				return -1;
+		}
+		int flags = fcntl(client->fd, F_GETFL, 0);
+		fcntl(client->fd, F_SETFL, flags | O_NONBLOCK);//set the socket as non-block 
+		return 0;
+	}
+}
+
+/*************************server part******************************************/
+int inet_server_config(int port, int proc_type, inet* server){
+	server->proc = proc_type;
+
+    switch(server->proc){
+	case INET_PROC_TCP:
+	    if((server->fd = socket(AF_INET, SOCK_STREAM, 0)) < 0){
+			return -1;
+	    }
+	    break;
+	case INET_PROC_UDP:
+	    if((server->fd = socket(AF_INET, SOCK_DGRAM, 0)) < 0){
+			return -1;
+	    }
+	    break;
+		//if unknown proc, return error
+	default:
+	    return -1;
+    }
+
+    int flags = fcntl(server->fd, F_GETFL, 0);
+    fcntl(server->fd, F_SETFL, flags | O_NONBLOCK);//set the socket as non-block 
+
+	server->ip_addr.sin_family = AF_INET;
+	server->ip_addr.sin_addr.s_addr = INADDR_ANY;
+    server->ip_addr.sin_port = htons(port);
+
+	//bind fd and ip_addr
+	if( bind(server->fd, (struct sockaddr *) &(server->ip_addr), sizeof(server->ip_addr)) < 0){
+		return -1;
+	}
+
+	return 0;
+}
+
+/***********close current socket and reset the server socket********************/
+int inet_server_close(inet* server){
+	int retval = 0;
+
+	if (close(server->fd) < 0){
+		retval = -1;
+	}
+	else {
+		//reset socket fd
+		switch(server->proc){
+			case INET_PROC_TCP:
+				if ( (server->fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+					retval = -1;
+				}
+				break;
+			case INET_PROC_UDP:
+				if ( (server->fd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+					retval = -1;
+				}
+				break;
+			default:
+				retval = -1;
+		}
+
+		int flags = fcntl(server->fd, F_GETFL, 0);
+		fcntl(server->fd, F_SETFL, flags | O_NONBLOCK);//set the socket as non-block 
+
+		//bind local addr with port
+		if( bind(server->fd, (struct sockaddr *) &(server->ip_addr), sizeof(server->ip_addr)) < 0){
+			retval = -1;
+		}
+		retval = 0;
+	}
+
 	return retval;
 }
--- twrt/src/simple_inet.h
+++ twrt/src/simple_inet.h
@@ -24,14 +24,10 @@ typedef struct{
     struct sockaddr_in ip_addr;
 }inet;
 
-//int inet_server_config(int port, int proc_type, struct_serial* inet_server);
-//int inet_server_open(struct_inet* inet_server);
-//int on_inet_server_rx(struct_inet* inet_server, char* buff);
-//int on_inet_server_tx(struct_inet* inet_client, char* buff, int len);
-//int on_inet_server_tx_ack(struct_inet* inet_client, char* bytes, int len, char* ack);
-//int inet_server_close(struct_inet* inet_server);
-
 int inet_client_config(char* ip, int port, int proc_type, inet* client);
 int inet_client_connect(inet* client);//for a long connection
 int inet_client_close(inet *client);
+
+int inet_server_config(int port, int proct_type, inet* server);
+int inet_server_close(inet* server);
 #endif
