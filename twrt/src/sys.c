6a49d5c5e50b479a98ff341dcda74bd83bc77a35
Lingfliu <lingfliu@gmail.com>
2015-09-04 13:51:13 +0800

    added localuser struct and localbundle for localuser tx

--- twrt/src/sys.c
+++ twrt/src/sys.c
@@ -1,7 +1,7 @@
 #include "sys.h"
 
-//znode operations
-/////////////////////////////////////////////////////
+/**znode operation************************************
+******************************************************/
 int znode_isempty(znode *znd){
     if(znd->status_len == 0 && znd->status == NULL && !memcmp(znd->id, NULL_DEV, MSG_LEN_ID_DEV)){
 		return 1;
@@ -28,74 +28,33 @@ void znode_create(znode* znd, message *msg){
     znd->ver = DEFAULT_VER;
     znd->status_len = msg->data_len;
     znd->status = realloc(znd->status, msg->data_len);
-	//printf("status_len = %d", msg->data_len);
     memcpy(znd->status, msg->data, msg->data_len);
-    znd->net_status = ZNET_ON;
+    znd->znet_status = ZNET_ON;
     znd->u_stamp = 0;
 }
 
 void znode_update(znode* znd, message *msg){
     memcpy(znd->status, msg->data, msg->data_len);
-    znd->net_status = ZNET_ON;
+    znd->znet_status = ZNET_ON;
     znd->u_stamp++;
 }
 
+//this function only remove the status of the znode
 void znode_flush(znode* znd){
     memset(znd->status, 0, znd->status_len);
-    free(znd->status);
+}
+
+//this function remove all infos of the znode
+void znode_delete(znode* znd){
+	free(znd->status);
     memset(znd, 0, sizeof(znode));
 }
-
-//sys_t operation
-//////////////////////////////////////////////////////
-void sys_get_id(sys_t* sys, char* id_file){
-    FILE* fp;
-    fp = fopen(id_file, "r");
-    if(fp == NULL){
-		perror("Cannot read id file, abort.");
-    }
-    if(fread(sys->id, sizeof(char), MSG_LEN_ID_GW, fp)!= MSG_LEN_ID_GW){
-		perror("read error");
-	}
-    fclose(fp);
-}
-
-void sys_get_lic(sys_t* sys, char* lic_file){
-    FILE *fp;
-    fp = fopen(lic_file, "r");
-    if(fp == NULL){
-		perror("Cannot read id file, abort.");
-    }
-    if(fread(sys->lic, sizeof(char), SYS_LEN_LIC, fp) != SYS_LEN_LIC){
-		perror("lic read error");
-	}
-    fclose(fp);
-}
-
-//initialize an empty sys_t
-void sys_init(sys_t* sys){
-	int m;
-    memset(sys, 0, sizeof(sys_t));
-
-    sys_get_id(sys, FILE_ID_SYS);
-    sys_get_lic(sys, FILE_LIC); 
-    sys->lic_status= LIC_UNKNOWN;
-    sys->server_status = SERVER_DISCONNECT;
-	sys->u_stamp = -1; //status stamp
-	sys->tx_msg_stamp = 0;
-
-	for(m = 0; m < PROC_ZNODE_MAX; m++){
-		memcpy(sys->znode_list[m].id, NULL_DEV, MSG_LEN_ID_DEV);
-		sys->znode_list[m].u_stamp = -1;
-	}	
-}
-
 
 int sys_znode_update(sys_t* sys, message* msg){
     int idx = -1;
     int idx_empty = -1;
     int m;
-	for(m = 0; m < PROC_ZNODE_MAX; m++){
+	for(m = 0; m < ZNET_SIZE; m++){
 		if(!memcmp(msg->dev_id, sys->znode_list[m].id, MSG_LEN_ID_DEV)){ //if id_dev equal
 			idx = m;
 			break;
@@ -103,14 +62,13 @@ int sys_znode_update(sys_t* sys, message* msg){
 	}
 
 	if( idx < 0 ) {//not in the list 
-		for(m = 0; m < PROC_ZNODE_MAX; m++ ) {
+		for(m = 0; m < ZNET_SIZE; m++ ) {
 			if(znode_isempty( &(sys->znode_list[m]) ) ){
 				idx_empty = m;
 				break;
 			}
 		}
-
-		if( idx_empty < 0 ) { //znode list is full, do nothing
+		if( idx_empty == -1 ) { //znode list is full, do nothing
 			return -1; 
 		}
 		else { //add new node in the list
@@ -128,25 +86,214 @@ int sys_znode_update(sys_t* sys, message* msg){
 int sys_get_znode_idx(sys_t *sys, char id[8]){
     int m;
     int idx = -1;
-    for(m = 0; m < PROC_ZNODE_MAX; m++){
-	if(!memcmp(sys->znode_list[m].id, id, MSG_LEN_ID_DEV)){
-	    idx = m;
-	    break;
-	}else{
-	    continue;
-	}
+    for(m = 0; m < ZNET_SIZE; m++) {
+		if(!memcmp(sys->znode_list[m].id, id, MSG_LEN_ID_DEV)) {
+			idx = m;
+			break;
+		}
+		else {
+			continue;
+		}
     }
     return idx;
 }
 
-int sys_get_znode_num(sys_t *sys){
+int sys_get_znode_num(sys_t *sys) {
     int num = 0;
     int m;
-    for(m = 0; m < PROC_ZNODE_MAX; m++){
+    for(m = 0; m < ZNET_SIZE; m++) {
 	if(!znode_isempty(&(sys->znode_list[m])))
 	    num++;
     }
     return num;
+}
+
+/**localuser operation*********************************
+******************************************************/
+int localuser_isnull(localuser *usr){
+    if(!memcmp(usr->id, NULL_USER, 8) || usr->skt < 0){
+		return 1;
+	}
+	else {
+		return 0;
+	}
+}
+
+int localuser_create(localuser *usr, char id[8], int skt){
+	//the thrd_rx and thrd_tx will not be activated here
+	//this function binds skt and user
+	memcpy(usr->id, id, 8*sizeof(char));
+	usr->skt = skt;
+
+	//initialize the timer
+	gettimeofday(&(usr->time_lastactive), NULL);
+
+	usr->is_authed = AUTH_OK;
+	return 0;
+}
+
+void localuser_delete(localuser *usr){
+	memcpy(usr->id, NULL_USER, 8*sizeof(char));
+	close(usr->skt);
+	usr->skt = -1;
+	usr->is_authed = AUTH_NO;
+	buffer_ring_byte_flush(&(usr->buff));
+}
+
+int sys_localuser_login(sys_t* sys, int skt){
+	int m;
+	int retval = -1;
+	for (m = 0; m < LOCALUSER_SIZE; m ++) {
+		if ( localuser_isnull( &(sys->localuser_list[m])) ) {
+			sys->localuser_list[m].skt = skt;
+			retval = m;
+			break;
+		}
+		else {
+			continue;
+		}
+	}
+
+	//if list is full, do the evict
+	if (retval == -1){
+		sys_localuser_evict(sys);
+		retval = sys_localuser_login(sys, skt); //and login again
+	}
+
+	return retval;
+} 
+
+//localuser logout (active logout)
+int sys_localuser_logout(sys_t* sys, char id[8]){
+	int idx = sys_get_localuser_idx(sys, id);
+	if (idx < 0) {
+		return -1;
+	}
+	else {
+		//close the socket and delete the user
+		localuser_delete( &(sys->localuser_list[idx]) );
+		return 0;
+	}
+}
+
+//force logout (passive logout) a localuser if: 1. localuser_list is full, 2. socket do not receive any data after timeout
+int sys_localuser_evict(sys_t* sys) {
+	int m;
+	int cnt;
+	long max_timediff;
+	int idx;
+	struct timeval timer;
+
+	gettimeofday(&timer, NULL);
+
+	cnt = 0;
+	for (m = 0; m < LOCALUSER_SIZE; m ++) {
+		if (timediff_s(sys->localuser_list[m].time_lastactive, timer) > DEFAULT_LOCALHOST_TIMEOUT) {
+			localuser_delete( &(sys->localuser_list[m]) );
+			cnt ++;
+		}
+	}
+
+	//if no user is timeouted, remove the oldest user
+	if (cnt == 0) {
+		idx = 0;
+		max_timediff = timediff_s(sys->localuser_list[0].time_lastactive, timer);
+		for (m = 1; m < LOCALUSER_SIZE; m ++) {
+			if (timediff_s(sys->localuser_list[m].time_lastactive, timer) > max_timediff) {
+				max_timediff = timediff_s(sys->localuser_list[m].time_lastactive, timer);
+				idx = m;
+			}
+		}
+		localuser_delete(&(sys->localuser_list[idx]));
+		cnt ++;
+	}
+	return cnt;
+}
+
+int sys_get_localuser_idx(sys_t* sys, char id[8]){
+	int idx = -1;
+	int m;
+	for(m = 0; m < LOCALUSER_SIZE; m ++) {
+		if(!memcmp(sys->localuser_list[m].id, id, 8)){
+			idx = m;
+			break;
+		}
+	}
+	return idx;
+}
+
+int sys_get_localuser_num(sys_t* sys){
+	int cnt = 0;
+	int m;
+	for(m = 0; m < LOCALUSER_SIZE; m ++) {
+		if(!localuser_isnull( &(sys->localuser_list[m])) ) {
+			cnt ++;
+		}
+	}
+	return cnt;
+}
+
+/************************************************
+  sys_t operation 
+ ************************************************/
+void sys_get_id(sys_t* sys, char* id_file){
+    FILE* fp;
+    fp = fopen(id_file, "r");
+    if(fp == NULL){
+		//perror("Cannot read id file, abort.");
+    }
+    if(fread(sys->id, sizeof(char), MSG_LEN_ID_GW, fp)!= MSG_LEN_ID_GW){
+		//perror("read error");
+	}
+    fclose(fp);
+}
+
+void sys_get_lic(sys_t* sys, char* lic_file){
+    FILE *fp;
+    fp = fopen(lic_file, "r");
+    if(fp == NULL){
+		//perror("Cannot read id file, abort.");
+    }
+    if(fread(sys->lic, sizeof(char), SYS_LEN_LIC, fp) != SYS_LEN_LIC){
+		//perror("lic read error");
+	}
+    fclose(fp);
+}
+
+//initialize an empty sys_t
+void sys_init(sys_t* sys){
+	int m;
+    memset(sys, 0, sizeof(sys_t));
+
+	//get id, authcode, and lic
+    sys_get_id(sys, FILE_ID_SYS);
+    sys_get_lic(sys, FILE_LIC); 
+
+	//reset status
+    sys->lic_status= LIC_UNKNOWN;
+    sys->server_status = SERVER_DISCONNECT;
+	sys->serial_status = SERIAL_OFF;
+	sys->local_status = LOCALHOST_CLOSED;
+
+	//initialize status stamp
+	sys->u_stamp = -1; 
+
+	//reset counters
+	sys->tx_msg_stamp = 0;
+
+	//initialize znet
+	for(m = 0; m < ZNET_SIZE; m++){
+		memcpy(sys->znode_list[m].id, NULL_DEV, MSG_LEN_ID_DEV);
+		sys->znode_list[m].u_stamp = -1;
+	}	
+
+	//initialize localuser
+	for(m = 0; m < LOCALUSER_SIZE; m++){
+		memcpy(sys->localuser_list[m].id, NULL_USER, 8);
+		buffer_ring_byte_create( &(sys->localuser_list[m].buff), BUFF_RING_LEN );
+		sys->localuser_list[m].skt = -1;
+		sys->localuser_list[m].is_authed = 0;
+	}
 }
 
 message* sys_sync(sys_t *sys, message *msg){
@@ -162,83 +309,22 @@ message* sys_sync(sys_t *sys, message *msg){
 	   }else if(sys->u_stamp == stamp){
 		   return NULL;
 	   }else{ //if local is newer, send back a sync msg
-		   return message_create_sync(SYS_LEN_STATUS, sys->status, sys->u_stamp, sys->id, NULL_DEV, 0, 0);
+		   return message_create_sync(SYS_LEN_STATUS, sys->status, sys->u_stamp, sys->id, NULL_DEV, 0);
 	   }
    }
-
-   idx = sys_get_znode_idx(sys, msg->dev_id);
-   if(idx<0) {
-	   return NULL;
-   }
-   if(sys->znode_list[idx].u_stamp < stamp) { //if msg contains newer status, update
-	   memcpy(sys->znode_list[idx].status, msg->data+4, msg->data_len-4);
-	   sys->znode_list[idx].u_stamp = stamp; //don't forget to update the stamp
-	   return NULL;
-   }
-   else { //if local status is newer, send back a sync msg
-	   return message_create_sync(sys->znode_list[idx].status_len, sys->znode_list[idx].status, sys->znode_list[idx].u_stamp, sys->id, sys->znode_list[idx].id, sys->znode_list[idx].type, 0);
+   else {
+	   idx = sys_get_znode_idx(sys, msg->dev_id);
+	   if(idx<0) {
+		   return NULL;
+	   }
+	   else if(sys->znode_list[idx].u_stamp <= stamp) { //if msg contains newer status, update
+		   memcpy(sys->znode_list[idx].status, msg->data+4, msg->data_len-4);
+		   sys->znode_list[idx].u_stamp = stamp; //don't forget to update the stamp
+		   return NULL;
+	   }
+	   else { //if local status is newer, send back a sync msg
+		   return message_create_sync(sys->znode_list[idx].status_len, sys->znode_list[idx].status, sys->znode_list[idx].u_stamp, sys->id, sys->znode_list[idx].id, sys->znode_list[idx].type);
+	   }
    }
 }
 
-void sys_save(sys_t *sys, char* save_file){
-    int m;
-    int fp;
-    fp = open(save_file, O_CREAT | O_WRONLY | O_TRUNC, S_IRWXU);
-    
-    if(fp == -1)
-	perror("save file open error");
-	//save the file as 
-	if(write(fp, &(sys->status), sizeof(sys->status)) != sizeof(sys->status)){
-		perror("write error");
-	}
-	if(write(fp, &(sys->u_stamp), sizeof(sys->u_stamp)) != sizeof(sys->u_stamp)){
-		perror("write error");
-	}
-	if(write(fp, sys->cookie, sizeof(sys->cookie)) != sizeof(sys->cookie)){
-		perror("write error");
-	}
-	for(m = 0; m<PROC_ZNODE_MAX; m++){
-		if(write(fp, sys->znode_list[m].id, MSG_LEN_ID_DEV) != MSG_LEN_ID_DEV){
-			perror("write error");
-		}
-		if(write(fp, &(sys->znode_list[m].type), sizeof(int)) != sizeof(int)){
-			perror("write error");
-		}
-		if(write(fp, &(sys->znode_list[m].u_stamp), sizeof(long)) != sizeof(long)){
-			perror("write error");
-		}
-	}
-
-}
-
-void sys_load(sys_t *sys, char* save_file){
-	int m;
-	int fp;
-	fp = open(save_file, O_RDONLY);
-
-	if(fp == -1)
-		perror("save file open error");
-
-	//save the file as 
-	if(read(fp, sys->status, SYS_LEN_STATUS) != SYS_LEN_STATUS){
-		perror("read error");
-	}
-	if(read(fp, &(sys->u_stamp), sizeof(long)) != sizeof(long)){
-		perror("read error");
-	}
-	if(read(fp, sys->cookie, SYS_LEN_COOKIE) != SYS_LEN_COOKIE){
-		perror("read error");
-	}
-
-	for(m = 0; m<PROC_ZNODE_MAX; m++){
-		if(read(fp, sys->znode_list[m].id, MSG_LEN_ID_DEV) != MSG_LEN_ID_DEV){
-			perror("read error");
-		}
-		if(read(fp, &(sys->znode_list[m].type), sizeof(int)) != sizeof(int)){
-			perror("read error");
-		}
-		if(read(fp, &(sys->znode_list[m].u_stamp), sizeof(long)) != sizeof(long)){
-			perror("read error");
-		}
-	}
-}
--- twrt/src/sys.h
+++ twrt/src/sys.h
@@ -5,23 +5,51 @@
 #include "proc.h"
 #include "simple_inet.h"
 #include "simple_serial.h"
+#include "buffer_ex.h"
+#include "utils.h"
 
 #include <string.h>
 #include <stdlib.h>
+#include <pthread.h>
 
+//sys status code
 #define SERVER_CONNECT 1
 #define SERVER_DISCONNECT 2
-#define SERVER_NORESPONSE 3
+
+#define LOCALHOST_LISTENING 1
+#define LOCALHOST_CLOSED 2 
+
+#define ZNET_ON 1
+#define ZNET_OFF 2
 
 #define SERIAL_ON 1
-#define SERIAL_OFF 0
+#define SERIAL_OFF 2
 
-#define SYS_REG_NO 0
-#define SYS_REG_OK 0
-
+//license status code
 #define LIC_VALID 1
 #define LIC_INVALID 2
 #define LIC_UNKNOWN 3
+
+//default auth code
+#define DEFAULT_AUTHCODE "TWRT2015"
+#define NULL_USER "00000000"
+
+//system size
+#define ZNET_SIZE 255 //maximum number of znodes in znet
+#define LOCALUSER_SIZE 3 //maximum number of concurrent localuser connection 
+
+//memory settings
+#define BUFF_IO_LEN 500 
+#define BUFF_RING_LEN 1000 
+#define MSG_Q_LEN 50  //length of message queue
+
+//timer settings
+#define TIMER_PULSE 1000 //pulse ack waiting time in milli-second
+#define TIMER_RESET 72 //pulse ack waiting time in hour 
+#define TIMER_REQ 2000 //request waiting time in milli-second
+#define TIMER_SYNC 30 //synchronization time in seconds 
+
+#define DEFAULT_LOCALHOST_TIMEOUT 10 //server socket timeout in seconds
 
 //sys and znode
 typedef struct{
@@ -31,19 +59,42 @@ typedef struct{
     int ver;//firmware version
     int status_len;
     char* status;
-    int net_status;
+    int znet_status; //znet status: on, off
     long u_stamp;//node stamp for status update
     long g_stamp;//extra stamp for status update (not used yet)
 }znode; 
 
-int znode_isempty(znode *znd);
-int znode_copy(znode* znode_dst, znode* znode_src);
+//znode operation
+int znode_isempty(znode *znd); //check if znode contains no znode info
+int znode_copy(znode* znode_dst, znode* znode_src); //hard copy of a znode
 void znode_create(znode* znd, message *msg); //create a znode given status msg
 void znode_update(znode* znd, message *msg); //update a znode given status msg
-void znode_flush(znode* znd);
-//znode opeation
-/////////////////////////////////////////////
+void znode_delete(znode* znd); //delete the znode (both status and other infos)
+void znode_flush(znode* znd); //flush the znode's status and u_stamp
 
+//local client
+typedef struct{
+	char id[8];
+	int skt;
+	pthread_t thrd_rx;
+	pthread_t thrd_tx;
+	char buff_io[BUFF_IO_LEN];
+	buffer_ring_byte buff;
+	int is_authed;
+    struct timeval time_lastactive; //timer to send tcp pulse to the server
+}localuser;
+
+int localuser_isnull(localuser *usr);
+int localuser_create(localuser *usr, char id[8], int skt);
+void localuser_delete(localuser *usr);
+
+//bundle for tx message of localuser
+typedef struct{
+	localuser *usr;
+	message *msg;
+}localbundle;
+
+/////////////////////////////////////////////
 typedef struct{
     //sys core
     char id[8];
@@ -52,46 +103,62 @@ typedef struct{
     char status[SYS_LEN_STATUS]; //root status of the sys_t
 
     //znet
-    znode znode_list[PROC_ZNODE_MAX]; //fixed length of znode_list
+    znode znode_list[ZNET_SIZE]; //fixed length of znode_list
+
+	//local client
+	localuser localuser_list[LOCALUSER_SIZE];
 
     //stamp for synchronization
     long u_stamp;//sys stamp for status update
     long g_stamp;//extra sys stamp for satus update (not used yet)
 
-    //stamp for tx msg
-    long tx_msg_stamp;
+	//counters
+    long tx_msg_stamp;//stamp for tx_msg
 
     //license
-    char lic[SYS_LEN_LIC];
-    char cookie[SYS_LEN_COOKIE];
+    char lic[SYS_LEN_LIC]; //lic should be a string code to activate the GW in the server
+    char auth_code[SYS_LEN_AUTHCODE]; //cookie should be a short auth code for GW identification and local operation
     int lic_status;
 
     //timer 
     struct timeval timer_pulse; //timer to send tcp pulse to the server
-    struct timeval  timer_reset; //timer to reset the sys_t 
+    struct timeval  timer_reset; //timer to reset the sys_t (not used)
     struct timeval  timer_sync; //timer to synchronization 
 
-    //server status
-    int server_status;
-
-	//serial status
-	int serial_status;
+	//connection status
+    int server_status;//server status
+	int serial_status;//serial status
+	int local_status;//localhost status
 }sys_t;
 
-//sys_t operation
 void sys_get_id(sys_t *sys, char* id_file); //get GW id from file
-
 void sys_get_lic(sys_t *sys, char* lic_file); //get license from file
-void sys_init(sys_t *sys); //initial empty sys_t
-void sys_reset(sys_t *sys); //reset & flush the sys_t to its initial status
 
+/*system initialization when program started
+todo:
+ 1. get id, lic
+ 2. initialize an empty znode_list
+ 3. initialize an empty localuser_list
+ 4. set tx_msg_stamp = 0
+ 5. set u_stamp = -1
+ 6. init timers as gettimeofday
+ 7. set connection status as disconnected & close*/
+void sys_init(sys_t *sys); 
+
+/*synchronize the sys_t based on sync msg, return sync msg (sync to server) or NULL*/
+message* sys_sync(sys_t *sys, message *msg); 
+
+//znode operation
 int sys_znode_update(sys_t* sys, message* msg); //update the znode status in sys_t, if znode is not found, add into the znode_list, return the index of the updated znode
-
 int sys_get_znode_idx(sys_t* sys, char id[8]); //return the znode index given id
-
 int sys_get_znode_num(sys_t* sys); //get the actual number of znodes in the list
-message* sys_sync(sys_t *sys, message *msg); //synchronize the sys_t by ack msg, return sync msg or NULL
 
-void sys_save(sys_t *sys, char* save_file); //save the current sys_t status to file 
-void sys_load(sys_t *sys, char* save_file); //save the current sys_t status to file 
+//localuser operation
+int sys_localuser_login(sys_t* sys, int skt); 
+int sys_localuser_logout(sys_t* sys, char id[8]); 
+int sys_localuser_evict(sys_t* sys); //force logout (passive logout) a localuser if: 1. localuser_list is full, 2. socket do not receive any data after timeout
+int sys_get_localuser_idx(sys_t* sys, char id[8]);
+int sys_get_localuser_num(sys_t* sys);
+
+
 #endif
