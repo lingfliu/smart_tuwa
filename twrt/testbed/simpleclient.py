062ffa6fc13dde2aeec51f48fadc18d2035ebeb6
Lingfliu <lingfliu@gmail.com>
2016-04-28 23:08:37 +0800

    added test script for scene update and scene control

--- twrt/testbed/simpleclient.py
+++ twrt/testbed/simpleclient.py
@@ -12,7 +12,6 @@ msg_auth_datatype = '\x1c\x00'
 
 msg_auth = msg_header+msg_stamp+msg_id_gw+msg_id_dev+msg_devtype+msg_auth_datatype+'\x00\x08'+msg_auth_key
 
-x = 5
 install_id = '00000000'
 install_type = '\x02\x00\x00\x00'
 install_name = 'switch001' + '\x00'*51
@@ -30,19 +29,23 @@ scene_id_major = '00000011'
 scene_id_minor = '00000001'
 scene_type = '\x01\x00\x00\x00'
 scene_name = 'scene'+'\x00'*55
-scene_trigger_num = 1
-scene_item_num = 1
-scene_trigger = '00000011'*2
-scene_item  = '00000012'*2
+
+scene_trigger1 = 'T0000001'*2
+scene_trigger2 = 'T0000001'*2
+scene_item1  = 'I0000001'*2
+scene_item2  = 'I0000002'*2
+
 scene_item_extra = '\x02\x00\x00\x00'*2
 
 scene_mac2 = '00000111'
 scene_id_major2 = '00000031'
 scene_id_minor2 = '00000011'
 
-msg_set_scene = msg_header+msg_stamp+msg_id_gw+msg_id_dev+msg_devtype+'\x0f\x00'+'\x00\x88' + scene_id_major+scene_id_minor+scene_mac+scene_type+scene_name+'\x01\x00\x00\x00'+'\x01\x00\x00\x00'+scene_trigger+scene_item_extra+scene_item+scene_item_extra
+msg_set_scene = msg_header+msg_stamp+msg_id_gw+msg_id_dev+msg_devtype+'\x0f\x00'+'\x00\x90' + scene_id_major+scene_id_minor+scene_mac+scene_type+scene_name+'\x01\x00\x00\x00'+'\x01\x00\x00\x00'+scene_trigger1+scene_item_extra+scene_item1+scene_item_extra
 
-msg_set_scene2 = msg_header+msg_stamp+msg_id_gw+msg_id_dev+msg_devtype+'\x0f\x00'+'\x00\x88' + scene_id_major2+scene_id_minor2+scene_mac2+scene_type+scene_name+'\x01\x00\x00\x00'+'\x01\x00\x00\x00'+scene_trigger+scene_item_extra+scene_item+scene_item_extra
+msg_update_scene = msg_header+msg_stamp+msg_id_gw+msg_id_dev+msg_devtype+'\x0f\x00'+'\x00\x90' + scene_id_major+scene_id_minor+scene_mac+scene_type+scene_name+'\x00\x00\x00\x00'+'\x02\x00\x00\x00'+scene_item1+scene_item_extra+scene_item2+scene_item_extra
+
+msg_set_scene2 = msg_header+msg_stamp+msg_id_gw+msg_id_dev+msg_devtype+'\x0f\x00'+'\x00\x90' + scene_id_major2+scene_id_minor2+scene_mac2+scene_type+scene_name+'\x01\x00\x00\x00'+'\x01\x00\x00\x00'+scene_trigger1+scene_item_extra+scene_item1+scene_item_extra
 
 msg_del_scene = msg_header+msg_stamp+msg_id_gw+msg_id_dev+msg_devtype+'\x12\x00'+'\x00\x10' + scene_id_major+scene_id_minor
 
@@ -52,6 +55,7 @@ msg_get_scene_null = msg_header+msg_stamp+msg_id_gw+msg_id_dev+msg_devtype+'\x0e
 
 msg_finish_scene = msg_header+msg_stamp+msg_id_gw+msg_id_dev+msg_devtype+'\x11\x00'+'\x00\x01' + '\x00'
 
+msg_scene_ctrl = msg_header+msg_stamp+msg_id_gw+msg_id_dev+msg_devtype+'\x0d\x00'+'\x00\x10'+scene_id_major+scene_id_minor
 LEN_BUFF_IO = 1000
 
 serverAddress = ('localhost', 9091)
@@ -60,25 +64,21 @@ skt = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 skt.connect(serverAddress)
 
 len = skt.send(msg_auth)
-print len
 msg_bak = skt.recv(1024)
 print msg_bak
 
-len = skt.send(msg_set_install)
-print len
-msg_bak = skt.recv(1024)
-print msg_bak
+#len = skt.send(msg_set_install)
+#msg_bak = skt.recv(1024)
+#print msg_bak
 
-len = skt.send(msg_get_install)
-print len
-msg_bak = skt.recv(1024)
-print msg_bak
+#len = skt.send(msg_get_install)
+#msg_bak = skt.recv(1024)
+#print msg_bak
 
 
-len = skt.send(msg_finish_install)
-print len
-msg_bak = skt.recv(1024)
-print msg_bak
+#len = skt.send(msg_finish_install)
+#msg_bak = skt.recv(1024)
+#print msg_bak
 
 len = skt.send(msg_set_scene)
 print len
@@ -91,12 +91,19 @@ print len
 msg_bak = skt.recv(1024)
 print msg_bak
 
-
-len = skt.send(msg_finish_scene)
+len = skt.send(msg_get_scene)
 print len
 msg_bak = skt.recv(1024)
-print 'finish scene result' + msg_bak
+print msg_bak
 
+
+len = skt.send(msg_scene_ctrl)
+print len
+
+len = skt.send(msg_update_scene)
+print len
+msg_bak = skt.recv(1024)
+print msg_bak
 
 len = skt.send(msg_get_scene)
 print len
@@ -104,16 +111,31 @@ msg_bak = skt.recv(1024)
 print msg_bak
 
 
-
-len = skt.send(msg_get_scene_null)
+len = skt.send(msg_scene_ctrl)
 print len
-msg_bak = skt.recv(1024)
-print msg_bak
 
 len = skt.send(msg_finish_scene)
 print len
 msg_bak = skt.recv(1024)
 print 'finish scene result' + msg_bak
 
-#while True:
-#	pass
+
+
+#len = skt.send(msg_set_scene1)
+#print len
+#msg_bak = skt.recv(1024)
+#print msg_bak
+
+
+
+
+#len = skt.send(msg_get_scene_null)
+#print len
+#msg_bak = skt.recv(1024)
+#print msg_bak
+
+#len = skt.send(msg_finish_scene)
+#print len
+#msg_bak = skt.recv(1024)
+#print 'finish scene result' + msg_bak
+
